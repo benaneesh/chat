86aab9d2d6137aa17147001ae4f8e158ee5033a3
benaneesh <benaneesh@gmail.com>
2013-06-04 01:55:28 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = '23ebde66c7915e4555d14c55e747ccf938945d08b639628ac1b69776673ddf013206fb59a28f54b45714abbcbce1672098676c82fdb16fc7ed9174249d7379b7'
